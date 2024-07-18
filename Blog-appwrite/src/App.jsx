fe37edfc2ebaade051fc38b708236c4f6d9fe6d8
Ankit-2145 <ankitsharma2615@gmail.com>
2024-07-18 20:32:42 +0530

    database functions created

--- Blog-appwrite/src/App.jsx
+++ Blog-appwrite/src/App.jsx
@@ -2,11 +2,11 @@ function App() {
 
   return (
     <>
-     
-      <h1 className="text-white bg-blue-500">A React X Appwrite Blog App</h1>
-     
+      <h1 className="p-4 text-center text-2xl italic text-white bg-blue-500">
+        A React X Appwrite Blog App
+      </h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
