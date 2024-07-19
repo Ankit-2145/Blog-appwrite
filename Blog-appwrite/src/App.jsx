fd02975e95d2396968167232f868e1aa8ebd40c0
Ankit-2145 <ankitsharma2615@gmail.com>
2024-07-19 14:38:55 +0530

    auth Layouted

--- Blog-appwrite/src/App.jsx
+++ Blog-appwrite/src/App.jsx
@@ -1,12 +1,38 @@
-function App() {
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
 
-  return (
-    <>
-      <h1 className="p-4 text-center text-2xl italic text-white bg-blue-500">
-        A React X Appwrite Blog App
-      </h1>
-    </>
-  );
+function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
