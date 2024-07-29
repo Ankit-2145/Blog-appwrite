b8d06e235ef643ac23b7542e81e9ed531743ca28
Ankit-2145 <ankitsharma2615@gmail.com>
2024-07-29 11:29:07 +0530

    bug fixes

--- Blog-appwrite/src/pages/Users.jsx
+++ Blog-appwrite/src/pages/Users.jsx
@@ -16,7 +16,7 @@ function AllPosts() {
   return (
     <div className="w-full py-8">
       <Container>
-        <div className="grid grid-cols-3">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
           {posts.map((post) => (
             <div key={post.$id} className="p-2 w-full">
               <UserCard {...post} />
